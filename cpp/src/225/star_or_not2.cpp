983ab4f5ca51614579ad32ab0bb65f27a00be3b6
RyotaBannai <ryotala0528@gmail.com>
2021-10-30 22:56:05 +0900

    add: star or not. didn't have to store value in matrix.

---
+++ cpp/src/225/star_or_not2.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define lp(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+
+auto main() -> int
+{
+  int N, a, b;
+  cin >> N;
+  vector<int> p(N);
+
+  lp(i, N - 1)
+  {
+    cin >> a >> b;
+    p[a - 1]++, p[b - 1]++;
+  }
+
+  cout << (find(p.begin(), p.begin() + N, N - 1) != p.begin() + N ? "Yes" : "No") << endl;
+}
