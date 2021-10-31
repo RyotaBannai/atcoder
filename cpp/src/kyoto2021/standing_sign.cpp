e1316fe50eda8c4f9c84965feba2433f289e7443
RyotaBannai <ryotala0528@gmail.com>
2021-10-31 17:16:20 +0900

    add: kupc2021 A standing sign

---
+++ cpp/src/kyoto2021/standing_sign.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define lp(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+int N;
+
+auto main() -> int
+{
+  int T;
+  cin >> N;
+  vector<int> boards(N);
+
+  lp(i, N) cin >> boards[i];
+  cin >> T;
+
+  sort(boards.begin(), boards.end());
+  int times = boards[N - 1] / T;
+  int fst = 0;
+  lp(i, times + 1)
+  {
+    // cout << boards[0] << " " << (i + 1) * T << endl;
+    if ((i + 1) * T > boards[0]) {
+      fst = i;
+      break;
+    }
+  }
+
+  // cout << fst << endl;
+  // cout << boards[N - 1] << endl;
+  // lp(i, N) cout << boards[i] << " ";
+  // cout << endl;
+  // lp(i, 7) cout << (i + 1) * T << " ";
+  // cout << endl;
+
+  cout << times + 1 - fst << endl;
+}
---
+++ cpp/src/kyoto2021/standing_sign2.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#define lp(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+int N;
+
+auto main() -> int
+{
+  int T;
+  cin >> N;
+  vector<int> boards(N);
+
+  lp(i, N) cin >> boards[i];
+  cin >> T;
+
+  sort(boards.begin(), boards.end());
+
+  bool standing = false;
+  int visits = 0;
+  for (int i = 1; i <= boards[N - 1]; i++) {
+    auto res = find_if(boards.begin(), boards.end(), [&](int x) { return x == i; });
+    standing |= res != boards.end();
+    if (i % T == 0 && standing) {
+      // cout << "in: " << i % T << endl;
+      standing = false, visits++;
+    }
+  }
+  cout << visits + (standing ? 1 : 0) << endl;
+}
