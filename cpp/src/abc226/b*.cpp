fa37684569489503c538e2f5250f86aac9d39d35
RyotaBannai <ryotala0528@gmail.com>
2021-11-07 23:26:58 +0900

    add: solution for B

---
+++ cpp/src/abc226/b*.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <set>
+#include <string>
+#define lp(i, n) for (int i = 0; i < n; i++)
+using namespace std;
+
+set<string> s;
+
+auto main() -> int
+{
+  int N;
+  string rest;
+  cin >> N;
+  getline(std::cin, rest);
+
+  lp(i, N)
+  {
+    string line;
+    getline(std::cin, line);
+    if (line[0] == '0' || line.length() <= 2)
+      continue;
+    s.insert(line.substr(2));
+  }
+
+  cout << s.size() << endl;
+}
