5ac0a47d598539057d9b6f83b978ea0ceb41f224
RyotaBannai <ryotala0528@gmail.com>
2021-11-14 23:52:06 +0900

    add: knapsack

---
+++ cpp/src/ant/dp/knapsack_naive.cpp
@@ -0,0 +1,44 @@
+/*
+in:
+4 5
+2 3
+1 2
+3 4
+2 2
+
+out:
+7
+*/
+#include <iostream>
+#include <vector>
+#define lp(i, n) for (int i = 0; i < n; i++)
+#define lps(i, j, n) for (int i = j; i < n; i++)
+using namespace std;
+
+static const int MAX_N = 100;
+int w[MAX_N], v[MAX_N];
+int n, W;
+
+auto rec(int i, int j) -> int
+{
+  int res;
+  if (i == n) {
+    res = 0;
+  }
+  else if (j < w[i]) {
+    res = rec(i + 1, j);
+  }
+  else {
+    res = max(rec(i + 1, j), rec(i + 1, j - w[i]) + v[i]);
+  }
+  return res;
+}
+
+auto main() -> int
+{
+  cin >> n >> W;
+  lp(i, n) cin >> w[i], cin >> v[i];
+
+  // memset(dp, -1, sizeof(dp));
+  cout << rec(0, W) << endl;
+}
