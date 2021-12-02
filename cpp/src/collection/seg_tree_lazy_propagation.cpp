99af0c29fdc90690e670ae50d94bbd83e0665726
RyotaBannai <ryotala0528@gmail.com>
2021-12-03 01:13:04 +0900

    add: add set,build,find_rightest,find_leftest methods.

--- cpp/src/collection/seg_tree_lazy_propagation.cpp
+++ cpp/src/collection/seg_tree_lazy_propagation.cpp
@@ -43,6 +43,7 @@ template <typename T> struct RUQ {
 
   auto left(int i) -> int { return i * 2 + 1; }
   auto right(int i) -> int { return i * 2 + 2; }
+  auto mid(int l, int r) -> int { return (l + r) / 2; }
 
   void eval(int k) // 配列のk番目を更新
   {
@@ -56,6 +57,22 @@ template <typename T> struct RUQ {
     lazy[k] = INF;
   }
 
+  /*
+  値の更新には O(log n) だけかかるため、 n 個の更新を行おうとすると O(nlog n) だけかかる
+  はじめに n 個の要素を葉にセットしてから、後で同時に更新することで、これを O(n)
+  に抑えることが可能.
+
+  i番目は、配列上では i+n-1 番目に格納されている
+  */
+  void set(int i, T x) { dat[i + n - 1] = x; }
+  void build()
+  // build := set で直接葉を更新した後に、まとめてセグ木の全体を min で更新
+  // n = 8 とした時、n-2 は一番後ろの内部節点なので、そこから根まで順に更新
+  {
+    for (int k = n - 2; k >= 0; k--)
+      dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
+  }
+
   void update(int a, int b, T x, int k, int l, int r)
   {
     eval(k);
@@ -64,8 +81,8 @@ template <typename T> struct RUQ {
       eval(k);
     }
     else if (a < r && l < b) { // 一部区間が被る時
-      update(a, b, x, left(k), l, (l + r) / 2);
-      update(a, b, x, right(k), (l + r) / 2, r);
+      update(a, b, x, left(k), l, mid(l, r));
+      update(a, b, x, right(k), mid(l, r), r);
       dat[k] = min(dat[left(k)], dat[right(k)]);
     }
   }
@@ -79,12 +96,66 @@ template <typename T> struct RUQ {
     else if (a <= l && r <= b)
       return dat[k];
     else {
-      T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
-      T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+      T vl = query_sub(a, b, left(k), l, mid(l, r));
+      T vr = query_sub(a, b, right(k), mid(l, r), r);
       return min(vl, vr);
     }
   }
   auto query(int a, int b) -> T { return query_sub(a, b, 0, 0, n); }
+
+  auto find_rightest(int a, int b, T x) -> int { return find_rightest_sub(a, b, x, 0, 0, n); }
+  auto find_leftest(int a, int b, T x) -> int { return find_leftest_sub(a, b, x, 0, 0, n); }
+  auto find_rightest_sub(int a, int b, T x, int k, int l, int r) -> int
+  {
+    // 自分の値が x より大きい or [a,b) が [l,r) の範囲外
+    if (dat[k] > x || r <= a || b <= l) {
+      return a - 1;
+    }
+    // 自分が葉ならその位置. dat の index ではなく、葉としての位置
+    else if (k >= n - 1) {
+      return (k - (n - 1));
+    }
+    else {
+      /*
+      一番右の x 以下の値を見たいため、右の部分木を先に見て見つけたらその時点で探索終了.
+      左の場合も同様
+      */
+      int vr = find_rightest_sub(a, b, x, right(k), mid(l, r), r);
+      /*
+      右の部分木を見て a-1 以外なら return
+      [a,b) の範囲で探索しているため、実際に値が見つかった時 a-1
+      が帰ることはないため異常値として使用
+
+      左の場合は b を異常値として使用
+      */
+      if (vr != a - 1) {
+        return vr;
+      }
+      else { // 左の部分木を見て値を return
+        return find_rightest_sub(a, b, x, left(k), l, mid(l, r));
+      }
+    }
+  }
+  auto find_leftest_sub(int a, int b, T x, int k, int l, int r) -> int
+  {
+    // 自分の値がxより大きい or [a,b)が[l,r)の範囲外
+    if (dat[k] > x || r <= a || b <= l) {
+      return b;
+    }
+    // 自分が葉ならその位置
+    else if (k >= n - 1) {
+      return (k - (n - 1));
+    }
+    else {
+      int vl = find_leftest_sub(a, b, x, left(k), l, mid(l, r));
+      if (vl != b) { // 左の部分木を見て b 以外ならreturn
+        return vl;
+      }
+      else { // 右の部分木を見て値をreturn
+        return find_leftest_sub(a, b, x, right(k), mid(l, r), r);
+      }
+    }
+  }
 
   /* debug */
   inline T operator[](int a) { return query(a, a + 1); }
