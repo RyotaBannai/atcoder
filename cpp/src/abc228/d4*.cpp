c58ec3e1fc5b760e8baee1686ff91e73b5582027
RyotaBannai <ryotala0528@gmail.com>
2021-12-06 23:11:26 +0900

    add: solusion using seg tree abc228 d

--- cpp/src/abc228/d4*.cpp
+++ cpp/src/abc228/d4*.cpp
@@ -6,6 +6,7 @@ Linear Probing
 #include <iostream>
 #include <vector>
 using namespace std;
+using ll = long long;
 
 constexpr int SIZE = 1 << 20;
 constexpr int MASK = SIZE - 1;
@@ -20,10 +21,21 @@ template <typename T> struct SegTree {
     while (leafs > x)
       x *= 2;
     n = x;
+
+    for (int i = 0; i < SIZE; i++)
+      set(i, i);
+    build();
   }
 
   auto left(int i) -> int { return i * 2 + 1; }
   auto right(int i) -> int { return i * 2 + 2; }
+
+  void set(int i, T x) { dat[i + n - 1] = x; }
+  void build()
+  {
+    for (int k = n - 2; k >= 0; k--)
+      dat[k] = min(dat[left(k)], dat[right(k)]);
+  }
 
   void update(int i, int v)
   {
@@ -35,24 +47,30 @@ template <typename T> struct SegTree {
     }
   }
 
-  inline auto operator[](int a) -> pair<int, T> { return query(a, a + 1); }
-  auto query(int a, int b) -> pair<int, T> { return query_sub(a, b, 0, 0, n); }
-  auto query_sub(int a, int b, int k, int l, int r) -> pair<int, T>
+  inline auto operator[](int a) -> int { return query(a, a + 1); }
+  auto query(int a, int b) -> int { return query_sub(a, b, 0, 0, n); }
+  auto query_sub(int a, int b, int k, int l, int r) -> int
   {
     if (r <= a || b <= l)
-      return make_pair(k, INF);
+      return INF;
     else if (a <= l && r <= b)
-      return make_pair(k, dat[k]);
+      return dat[k];
     else {
-      int kl = left(k), kr = right(k);
-      pair<int, T> vl = query_sub(a, b, kl, l, (l + r) / 2);
-      pair<int, T> vr = query_sub(a, b, kr, (l + r) / 2, r);
-
-      if (vl.second <= vr.second)
-        return make_pair(kl, vl.second);
-      else
-        return make_pair(kr, vr.second);
+      int vl = query_sub(a, b, left(k), l, (l + r) / 2);
+      int vr = query_sub(a, b, right(k), (l + r) / 2, r);
+      return min(vl, vr);
     }
+  }
+  /* debug */
+  void print(int end = 0)
+  {
+    end = end ?: 2 * n - 1;
+    for (int i = 0; i < end; ++i) {
+      cout << (*this)[i].second;
+      if (i != n)
+        cout << ",";
+    }
+    cout << endl;
   }
 };
 
@@ -60,22 +78,24 @@ auto main() -> int
 {
   int q;
   cin >> q;
-  vector<long long> value(SIZE, INF);
-  auto seg = make_unique<SegTree<int>>(SIZE);
+  vector<ll> value(SIZE, -1);
+  auto seg = make_unique<SegTree<ll>>(SIZE);
   while (q--) {
     int t;
-    long long x;
+    ll x;
     cin >> t >> x;
     if (t == 1) {
       int h = x & MASK;
-      pair<int, int> re = seg->query(h, SIZE); // pair<index, value>
-      if (re.second != INF)
-        seg->update(re.first, INF);
-      cout << h << ", " << x << ", fst: " << re.first << endl;
-      value[h] = x;
+      int re = seg->query(h, SIZE);
+      if (re == INF)
+        re = seg->query(0, h);
+
+      if (re != INF) {
+        seg->update(re, INF);
+        value[re] = x;
+      }
     }
-    else {
+    else
       cout << value[x & MASK] << '\n';
-    }
   }
 }
