ea36b39d04b7db38b5870abb96d79584f5b0a157
RyotaBannai <ryotala0528@gmail.com>
2021-12-06 23:23:14 +0900

    refactor: duplicate codes.

--- cpp/src/abc228/d*.cpp
+++ cpp/src/abc228/d*.cpp
@@ -40,16 +40,10 @@ auto main() -> int
       // lower_bound() の結果が s.end() になる.
       // その際は h++ をして次の最小を見つけるように
       // 最も小さい数(ここでは 0)から次の位置を見るけるようにする.
-      if (pos == s.end()) {
+      if (pos == s.end())
         pos = s.lower_bound(0);
-        // 一度見つけたら値を入れ、set から削除.
-        x[*pos] = v;
-        s.erase(*pos);
-      }
-      else {
-        x[*pos] = v;
-        s.erase(*pos);
-      }
+      x[*pos] = v;
+      s.erase(*pos); // 見つけたら値を入れ、二度取り出さないように set から削除.
     }
     else {
       cout << x[v % N] << endl;
