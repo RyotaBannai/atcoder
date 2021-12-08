053b2875944424174d716f3cc0a28166e4cd5d1b
RyotaBannai <ryotala0528@gmail.com>
2021-12-08 23:45:00 +0900

    add: naive sum impl

--- cpp/src/collection/bit/bit.cpp
+++ cpp/src/collection/bit/bit.cpp
@@ -6,6 +6,12 @@ Binary Indexed Tree(BIT)
 #include <vector>
 using namespace std;
 
+/* BIT: 区間和の更新や計算を行う構造体
+    初期値は a_1 = a_2 = ... = a_n = 0
+    ・add(i,x): a_i += x とする
+    ・sum(i): a_1 + a_2 + ... + a_i を計算する
+    計算量は全て O(logn)
+*/
 template <typename T> struct BIT {
   int n;
   vector<T> bit;
@@ -20,6 +26,18 @@ template <typename T> struct BIT {
     for (int idx = i; idx < n; idx += (idx & -idx)) {
       bit[idx] += x;
     }
+  }
+
+  /*
+  i から初めて最後に 1 が立っているビットを減算しながら、i の値を合計していく
+  */
+  T sum(int i)
+  {
+    T s(0);
+    for (int idx = i; idx > 0; idx -= (idx & -idx)) {
+      s += bit[idx];
+    }
+    return s;
   }
 };
 auto main() -> int {}
