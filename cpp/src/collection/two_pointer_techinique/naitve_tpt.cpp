33ab094a89a8bc85d0de404ad57895372dd1060c
RyotaBannai <ryotala0528@gmail.com>
2021-12-22 00:05:59 +0900

    add: in/out

--- cpp/src/collection/two_pointer_techinique/naitve_tpt.cpp
+++ cpp/src/collection/two_pointer_techinique/naitve_tpt.cpp
@@ -2,6 +2,17 @@
 n=12
 a=(4,6,7,8,1,2,110,2,4,12,3,9)
 x=25
+
+input:
+12 1
+4 6 7 8 1 2 110 2 4 12 3 9
+25
+
+output:
+32
+
+各 left に対して最悪 O(n)O(n) かかるので全体で O(n2)O(n2)
+
 */
 #include <iostream>
 #include <vector>
@@ -30,7 +41,7 @@ auto main() -> int
       /* sum に a[right] を加えても大丈夫なら right を動かす */
       while (right < n && sum + a[right] <= x) {
         sum += a[right];
-        ++right;
+        right++;
       }
 
       /* break した状態で right は条件を満たす最大*/
