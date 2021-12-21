edfb1fda7e594d4df4f6c6ae9ab14476ce8d16b0
RyotaBannai <ryotala0528@gmail.com>
2021-12-21 23:25:17 +0900

    add: Two-pointer techinique

---
+++ cpp/src/collection/two_pointer_techinique/naitve_tpt.cpp
@@ -0,0 +1,42 @@
+/*
+n=12
+a=(4,6,7,8,1,2,110,2,4,12,3,9)
+x=25
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+auto main() -> int
+{
+  int n, Q;
+  cin >> n >> Q;
+  vector<ll> a(n);
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  for (int j = 0; j < Q; j++) {
+    ll x;
+    cin >> x;
+
+    // 合計値
+    ll res = 0;
+
+    for (int left = 0; left < n; left++) {
+      ll sum = 0;
+      int right = left; // [left, right) の総和が x 以下となる最大の right を求める
+
+      /* sum に a[right] を加えても大丈夫なら right を動かす */
+      while (right < n && sum + a[right] <= x) {
+        sum += a[right];
+        ++right;
+      }
+
+      /* break した状態で right は条件を満たす最大*/
+      res += (right - left);
+    }
+
+    cout << res << endl;
+  }
+}
