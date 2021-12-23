c40b7cebdcd31a0d777a98be7d478b1523f7f9a9
RyotaBannai <ryotala0528@gmail.com>
2021-12-24 02:23:15 +0900

    add: two pointer tech and comb, TLE ver.

--- cpp/src/abc229/d.cpp
+++ cpp/src/abc229/d.cpp
@@ -1,30 +1,84 @@
 /*
 Longest X
 */
+
+#include <functional>
 #include <iostream>
 #include <vector>
 using namespace std;
+using ll = long long;
 
-string S;
-int K;
-
-auto rec(string s, int idx, int cnt, int k) -> int
+template <class T>
+void combination(const vector<T> &seed, int target_size, std::function<void(vector<T>)> f)
 {
-  if (s.size() <= idx)
-    return cnt;
+  if (target_size == 0) {
+    f(vector<int>{});
+    return;
+  }
 
-  int whenX = 0, whenNotX = 0;
-  if (s[idx] == 'X') {
-    whenX = rec(s, idx + 1, cnt + 1, k);
+  vector<int> indices(target_size);
+  const int seed_size = seed.size();
+  int start_index = 0;
+  int size = 0;
+
+  while (size >= 0) {
+    for (int i = start_index; i < seed_size; ++i) {
+      indices[size++] = i;
+      if (size == target_size) {
+        vector<T> comb(target_size);
+        for (int x = 0; x < target_size; ++x) {
+          comb[x] = seed[indices[x]];
+        }
+        f(comb);
+        break;
+      }
+    }
+    --size;
+    if (size < 0)
+      break;
+    start_index = indices[size] + 1;
   }
-  else if (k > 0) {
-    whenNotX = max(rec(s, idx + 1, cnt + 1, k - 1), rec(s, idx + 1, 0, k));
-  }
-  return max(cnt, max(whenX, whenNotX));
 }
 
 auto main() -> int
 {
-  cin >> S >> K;
-  cout << rec(S, 0, 0, K) << endl;
+  string a;
+  int s, n;
+  cin >> a >> s;
+  n = a.length();
+  int ans = 0;
+
+  vector<int> p;
+  for (int i = 0; i < n; i++) {
+    if (a[i] == '.')
+      p.push_back(i);
+  }
+
+  // 操作行う対象が無い
+  if (p.size() == 0) {
+    cout << n << endl;
+    exit(0);
+  }
+
+  combination<int>(p, s, [&ans, a, n](vector<int> comb) {
+    string a_ = a;
+    for (auto i : comb)
+      a_[i] = 'X';
+
+    int res = 0;
+    int right = 0;
+    for (int left = 0; left < n; left++) {
+      while (right < n && a_[right] == 'X') {
+        right++;
+      }
+
+      res = max(res, right - left);
+      if (right == left)
+        right++;
+    }
+
+    ans = max(ans, res);
+  });
+
+  cout << ans << endl;
 }
