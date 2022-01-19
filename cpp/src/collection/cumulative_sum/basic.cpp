1ba225099388c70e31b87d1a0e9cfbf719518b1b
RyotaBannai <ryotala0528@gmail.com>
2022-01-19 23:06:41 +0900

    add: added basic

---
+++ cpp/src/collection/cumulative_sum/basic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+const long long INF = 1LL << 60; // 仮想的な無限大の値
+
+auto main() -> int
+{
+  int N, Q;
+  cin >> N >> Q; // 配列サイズ
+  vector<int> a(N);
+  for (int i = 0; i < N; ++i)
+    cin >> a[i]; // a の入力
+
+  // 累積和
+  vector<int> s(N + 1, 0); // s[0] = 0 になる
+  for (int i = 0; i < N; ++i)
+    s[i + 1] = s[i] + a[i];
+
+  while (Q--) {
+    // 区間 [left, right) の総和を求める
+    int left, right;
+    cin >> left >> right;
+    cout << s[right] - s[left] << endl;
+  }
+}
