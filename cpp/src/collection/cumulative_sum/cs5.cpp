7cd3856182457b40894cb4ade60e4437cfc30b52
RyotaBannai <ryotala0528@gmail.com>
2022-01-25 23:57:38 +0900

    add: 2-d cumulative sum wip.

---
+++ cpp/src/collection/cumulative_sum/cs5.cpp
@@ -0,0 +1,14 @@
+/*
+ */
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+auto main() -> int
+{
+  // 入力 H x W のグリッド内の操作を累積和で管理
+  int H, W;
+  cin >> H >> W;
+  int a[H][W];
+}
