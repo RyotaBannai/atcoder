53f7277fe581242822c82413a247bee84c05a730
RyotaBannai <ryotala0528@gmail.com>
2022-01-26 00:23:27 +0900

    add: 2-d bit impl and test cases.

--- cpp/src/collection/cumulative_sum/cs5.cpp
+++ cpp/src/collection/cumulative_sum/cs5.cpp
@@ -11,4 +11,26 @@ auto main() -> int
   int H, W;
   cin >> H >> W;
   int a[H][W];
+
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      cin >> a[i][j];
+    }
+  }
+
+  int s[H + 1][W + 1];
+  memset(s, 0, sizeof s);
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      s[i + 1][j + 1] = s[i + 1][j] + s[i][j + 1] - s[i][j] + a[i][j];
+    }
+  }
+
+  int Q;
+  cin >> Q;
+  while (Q--) {
+    int x1, x2, y1, y2;
+    cin >> x1 >> x2 >> y1 >> y2;
+    cout << (s[x2][y2] - s[x1][y2] - s[x2][y1] + s[x1][y1]) << endl;
+  }
 }
