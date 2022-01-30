060de97d99e32c5def8f7d4a2b7c951ce92c4dba
RyotaBannai <ryotala0528@gmail.com>
2022-01-31 00:51:36 +0900

    add: solusion for C

---
+++ cpp/src/abc237/c*.cpp
@@ -0,0 +1,44 @@
+/* @cpg_dirspec c
+ */
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+auto main() -> int
+{
+  string s;
+  cin >> s;
+  int N = s.length();
+
+  int i = 0;
+  for (; i < N; i++) {
+    if (s[i] != 'a') {
+      break;
+    }
+  }
+
+  int j = N - 1;
+  for (; j >= 0; j--) {
+    if (s[j] != 'a') {
+      break;
+    }
+  }
+
+  // N == 1 の時、j==-1
+  if (i > N - (j + 1)) { // 前についている数が後方よりすでに多い時はだめ
+    cout << "No" << endl;
+    exit(0);
+  }
+
+  bool ans = true;
+  for (; i < j; i++, j--) {
+    if (s[i] != s[j]) {
+      ans = false;
+      break;
+    }
+  }
+
+  cout << (ans ? "Yes" : "No") << endl;
+}
