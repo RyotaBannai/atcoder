ced2a2c178997621d611c75e08a1160b17e07f8f
RyotaBannai <ryotala0528@gmail.com>
2022-02-09 00:25:19 +0900

    add: abc229 Longest X

--- cpp/src/abc229/d*.cpp
+++ cpp/src/abc229/d*.cpp
@@ -1,27 +1,29 @@
-/*
+/* @cpg_dirspec d
 Longest X
 
-Official ver.
+. の累積和を利用
+X をどこに埋めるかではなく、. が許容値 k を超えない範囲でチェック範囲を伸縮させながらテスト
+→ 累積和
 */
-
 #include <iostream>
 #include <vector>
 using namespace std;
 using ll = long long;
+
 auto main() -> int
 {
   string s;
-  cin >> s;
   ll k;
-  cin >> k;
+  cin >> s >> k;
   ll n = s.size();
   vector<ll> cnt(n + 1); // 累積和
+  // cnt[r] - cnt[l] で s[l,r) の '.' の数を表す
   for (ll i = 0; i < n; i++) {
     if (s[i] == '.')
       cnt[i + 1] = cnt[i] + 1;
     else
       cnt[i + 1] = cnt[i];
-  } // cnt[r] - cnt[l] で s[l,r) の '.' の数を表す
+  }
 
   // debug
   // for (auto x : cnt)
@@ -31,7 +33,9 @@ auto main() -> int
   ll ans = 0;
   ll r = 0;
   for (ll l = 0; l < n; l++) {
-    while (r < n && cnt[r + 1] - cnt[l] <= k) { // 累積和の [l,r) を使う. 次の要素との差分をテスト
+    // 累積和の [l,r) を使う. 次の要素との差分をテスト
+    // r+1 で先に超えないかチェックしてから increment
+    while (r < n && cnt[r + 1] - cnt[l] <= k) {
       r++;
     }
     // a=XX...X.X.X., s=2 のとき r=4 でbreak
