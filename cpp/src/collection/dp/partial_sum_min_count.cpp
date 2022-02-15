66fb7d52cef8ab35b70820472af0d194f083b08d
RyotaBannai <ryotala0528@gmail.com>
2022-02-16 00:41:31 +0900

    add: partial sum min count solution

--- cpp/src/collection/dp/partial_sum_min_count.cpp
+++ cpp/src/collection/dp/partial_sum_min_count.cpp
@@ -7,10 +7,15 @@
  * 選ぶ整数の個数の最小値を求めよ。
  * A にすることができない場合は -1 と出力せよ。
  *
+ * 前回と同じ位置と、今回の品物を使った場合とで比較して最小の方を使って随時更新
  */
 
+#include <iomanip> // std::setw(int), std::setfill(char)
+#include <ios>     // std::left, std::right
 #include <iostream>
+#include <vector>
 using namespace std;
+static const int HUGE = 1 << 29;
 
 auto main() -> int
 {
@@ -18,9 +23,10 @@ auto main() -> int
   int n, A;
   cin >> n >> A;
 
-  int dp[n + 1][A + 10]; // 多めにとる
-  memset(dp, false, sizeof(dp));
-  dp[0][0] = true; // 忘れない
+  // https://atcoder.jp/contests/APG4b/tasks/APG4b_t
+  vector<vector<int>> dp(n + 1, vector<int>(A + 10, HUGE));
+
+  dp[0][0] = 0; // 忘れない
 
   int as[A + 1];
   for (int i = 0; i < n; ++i) {
@@ -30,21 +36,21 @@ auto main() -> int
   for (int i = 0; i < n; i++) {
     for (int a = 0; a <= A; a++) {
       // いったん前の状態を無条件に引き継ぐため条件の中に入れない
-      dp[i + 1][a] += dp[i][a];
+      dp[i + 1][a] = dp[i][a];
 
       if (as[i] <= a) {
-        dp[i + 1][a] += min(dp[i + 1][a], dp[i][a - as[i]] + 1);
+        dp[i + 1][a] = min(dp[i + 1][a], dp[i][a - as[i]] + 1);
       }
     }
   }
 
   // debug
-  for (int i = 0; i <= n; i++) {
-    for (int a = 0; a <= A + 1; a++) {
-      cout << dp[i][a] << " ";
-    }
-    cout << endl;
-  }
+  // for (int i = 0; i <= n; i++) {
+  //   for (int a = 0; a <= A + 1; a++) {
+  //     cout << std::right << std::setw(10) << dp[i][a];
+  //   }
+  //   cout << endl;
+  // }
 
-  cout << (dp[n][A] ? dp[n][A] : -1) << endl;
+  cout << (dp.at(n).at(A) == HUGE ? -1 : dp[n][A]) << endl;
 }
