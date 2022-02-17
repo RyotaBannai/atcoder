b97011db5e98602f799715f5eeeaf5b46b29f3fb
RyotaBannai <ryotala0528@gmail.com>
2022-02-18 00:18:45 +0900

    add: partial sum with n items solution

--- cpp/src/collection/dp/partial_sum_with_n_items.cpp
+++ cpp/src/collection/dp/partial_sum_with_n_items.cpp
@@ -1,10 +1,15 @@
 /**
- * 最小個数部分和問題
- * https://algo-method.com/tasks/311
+ * K個以内部分和問題
+ * https://algo-method.com/tasks/312
  *
  * n 個の正の整数 a[0],a[1],…,a[n−1] と正の整数 A が与えられる。
  * これらの整数から K 個以内の整数を選んで総和が A になるようにすることが可能か判定せよ。
  * 可能ならば "YES" と出力し、不可能ならば "NO" と出力せよ。
+ *
+ * 初回のみ K-1 を入れる
+ * それ以外は i 番目の item を引いた数値が
+ * 　-1 なら不可能なので処理しない
+ * 　それ以外なら使用回数を 1 減らして、その数値かそれまでの使用回数の最大をとる
  */
 
 #include <iomanip> // std::setw(int), std::setfill(char)
@@ -35,23 +40,28 @@ auto main() -> int
       dp[i + 1][m] = dp[i][m];
 
       if (as[i] <= m) {
-        if (dp[i][m - as[i]] == -1) {
-          dp[i + 1][m] = K;
+        // i 番目の item で m を作成できるなら最小単位で作れるため K-1（初回のみ）
+        // i.g. m=10 の時, as[i]=10 なら 3+7 でよりも最小で m を作ることができる
+        if (as[i] == m) {
+          dp[i + 1][m] = K - 1;
         }
-        else {
-          dp[i + 1][m] = dp[i][m - as[i]] - 1;
+        // なんらかの組み合わせで作れるならば
+        // ただし 0 の場合は今回の使用で -1 になるため使えない m として設定
+        // 今回の使用で使う個数と前回までで使える個数の最大をとる
+        else if (dp[i][m - as[i]] != -1) {
+          dp[i + 1][m] = max(dp[i + 1][m], dp[i][m - as[i]] - 1);
         }
       }
     }
   }
 
   // debug
-  for (int i = 0; i <= n; i++) {
-    for (int m = 0; m <= M + 1; m++) {
-      cout << std::right << std::setw(10) << dp[i][m];
-    }
-    cout << endl;
-  }
+  // for (int i = 0; i <= n; i++) {
+  //   for (int m = 0; m <= M + 1; m++) {
+  //     cout << std::right << std::setw(10) << dp[i][m];
+  //   }
+  //   cout << endl;
+  // }
 
-  // cout << (dp.at(n).at(M) == -1 ? "No" : "Yes") << endl;
+  cout << (dp.at(n).at(M) == -1 ? "No" : "Yes") << endl;
 }
