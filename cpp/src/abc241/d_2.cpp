ef61fbfa3d155e1620622cc32d77cb1fcae44b22
RyotaBannai <ryotala0528@gmail.com>
2022-02-27 01:50:53 +0900

    add: map doesn't help

--- cpp/src/abc241/d_2.cpp
+++ cpp/src/abc241/d_2.cpp
@@ -1,91 +1,50 @@
 /* @cpg_dirspec d
 
-TLE
  */
+#include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
 using namespace std;
 using ll = long long;
-
-template <class T, class Compare = std::less<T>> struct sorted_vector {
-  vector<T> V;
-  Compare cmp;
-  typedef typename vector<T>::iterator iterator;
-  typedef typename vector<T>::const_iterator const_iterator;
-  iterator begin() { return V.begin(); }
-  iterator end() { return V.end(); }
-  const_iterator begin() const { return V.begin(); }
-  const_iterator end() const { return V.end(); }
-  sorted_vector(const Compare &c = Compare()) : V(), cmp(c) {}
-  template <class InputIterator>
-  sorted_vector(InputIterator first, InputIterator last, const Compare &c = Compare())
-      : V(first, last), cmp(c)
-  {
-    std::sort(begin(), end(), cmp);
-  }
-  iterator insert(const T &t)
-  {
-    iterator i = lower_bound(begin(), end(), t, cmp);
-    // if (i == end() || cmp(t, *i))
-    V.insert(i, t);
-    return i;
-  }
-  const_iterator find(const T &t) const
-  {
-    const_iterator i = lower_bound(begin(), end(), t, cmp);
-    return i == end() || cmp(t, *i) ? end() : i;
-  }
-};
+using M = multimap<int, int>;
 
 auto main() -> int
 {
-  ll N;
-  sorted_vector<ll> s;
+  int N;
+  M m;
   cin >> N;
+  int x, k;
 
   while (N--) {
     int q;
     cin >> q;
     if (q == 1) {
-      ll x;
       cin >> x;
-      s.insert(x);
+      m.emplace(x, x);
     }
     else if (q == 2) {
-      ll x, k;
       cin >> x >> k;
-      auto it = upper_bound(s.begin(), s.end(), x); // x より小さい
-      bool over = false;
-      while (k--) {
-        if (it == s.begin()) {
-          over = true;
-          break;
-        }
-        else {
-          it--;
-        }
+      auto it = m.upper_bound(x); // x より小さい
+
+      if (distance(m.begin(), it) < k) {
+        cout << -1 << endl;
       }
-      cout << (over ? -1 : *it) << endl;
-      // if (distance(s.begin(), it) < k) {
-      //   cout << -1 << endl;
-      // }
-      // else {
-      //   advance(it, -k);
-      //   cout << *it << endl;
-      // }
-      /** distance は ok */
+      else {
+        advance(it, -k);
+        cout << it->first << endl;
+      }
     }
     else if (q == 3) {
-      ll x, k;
       cin >> x >> k;
-      auto it = lower_bound(s.begin(), s.end(), x); // x より大きい
-      if (distance(it, s.end()) < k) {
+      auto it = m.lower_bound(x); // x より大きい
+      if (distance(it, m.end()) < k) {
         cout << -1 << endl;
       }
       else {
         advance(it, k - 1);
-        cout << *it << endl;
+        cout << it->first << endl;
       }
     }
   }
