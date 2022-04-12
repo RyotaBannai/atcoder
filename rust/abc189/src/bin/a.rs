d0bbb634f54b0ae156d7c98dcd3fcb694aa29f91
RyotaBannai <ryotala0528@gmail.com>
2022-04-13 00:13:09 +0900

    add: abc180 a

--- rust/abc189/src/bin/a.rs
+++ rust/abc189/src/bin/a.rs
@@ -1,3 +1,12 @@
 pub fn main() {
-    todo!()
+    proconio::input! {
+        s:String
+    }
+
+    let vec: Vec<char> = s.chars().collect();
+    if vec[0] == vec[1] && vec[1] == vec[2] && vec[2] == vec[0] {
+        println!("Won")
+    } else {
+        println!("Lost")
+    }
 }
