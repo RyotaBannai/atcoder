47394e735325bcd62f9772e2ad5a0b3ddaf8708f
RyotaBannai <ryotala0528@gmail.com>
2022-04-15 02:40:12 +0900

    add: abc175 a

--- rust/abc175/src/bin/a.rs
+++ rust/abc175/src/bin/a.rs
@@ -1,3 +1,16 @@
 pub fn main() {
-    todo()!;
+    proconio::input! {
+      s: proconio::marker::Chars,
+    }
+    let mut tmp = 0;
+    let mut ans = 0;
+    for c in s {
+        if c == 'R' {
+            tmp += 1;
+        } else {
+            tmp = 0;
+        }
+        ans = std::cmp::max(ans, tmp);
+    }
+    println!("{}", ans);
 }
