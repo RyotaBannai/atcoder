e30f328e2b57e3a13e18f8dd2dff9ad7645c01c9
RyotaBannai <ryotala0528@gmail.com>
2022-04-17 23:15:14 +0900

    add: until read inputs

--- rust/abc226/src/bin/c.rs
+++ rust/abc226/src/bin/c.rs
@@ -1,3 +1,22 @@
+/**
+ * Martial arts
+ *
+ * https://atcoder.jp/contests/abc226/tasks/abc226_c
+ */
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    }
+
+    let mut v: Vec<Vec<i32>> = vec![vec![]; n];
+    for i in 1..=n {
+        proconio::input! {
+            no: String,
+            num: usize,
+            a: [i32; num]
+         }
+        println!("{}, {}, {:?}", no, num, a);
+    }
 }
