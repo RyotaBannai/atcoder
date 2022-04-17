9dae4a2a41e6a64d940cf69b31f13bd03f440035
RyotaBannai <ryotala0528@gmail.com>
2022-04-18 00:10:53 +0900

    add: abc226 written in Rust

--- rust/abc226/src/bin/c.rs
+++ rust/abc226/src/bin/c.rs
@@ -4,19 +4,44 @@
  * https://atcoder.jp/contests/abc226/tasks/abc226_c
  */
 use proconio::input;
+use std::collections::VecDeque;
 
 fn main() {
     input! {
         n: usize
     }
 
-    let mut v: Vec<Vec<i32>> = vec![vec![]; n];
+    let mut v = vec![vec![]; n + 1];
+    let mut t = vec![0; n + 1];
     for i in 1..=n {
         proconio::input! {
-            no: String,
+            time: usize,
             num: usize,
-            a: [i32; num]
-         }
-        println!("{}, {}, {:?}", no, num, a);
+            a: [usize; num]
+        }
+        t[i] = time;
+        v[i].extend(a);
     }
+    let mut used = vec![false; n + 1];
+    let mut q: VecDeque<usize> = VecDeque::new();
+    q.push_back(n);
+    used[n] = true;
+
+    let mut sum = 0;
+    loop {
+        match q.pop_back() {
+            Some(i) => {
+                sum += t[i];
+                for &x in &v[i] {
+                    if !used[x] {
+                        q.push_back(x);
+                        used[x] = true; // 一度取得した技は修練しない
+                    }
+                }
+            }
+            None => break,
+        }
+    }
+
+    println!("{}", sum);
 }
