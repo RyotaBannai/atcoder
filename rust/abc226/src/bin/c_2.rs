69b1e639a0c23a4c47dcc14a6d27e237bbf0d7c6
RyotaBannai <ryotala0528@gmail.com>
2022-04-19 23:00:20 +0900

    add: abc226 C using refcell ver.

---
+++ rust/abc226/src/bin/c_2.rs
@@ -0,0 +1,48 @@
+/**
+ * Martial arts
+ *
+ * https://atcoder.jp/contests/abc226/tasks/abc226_c
+ * 
+ * using refcell ver.
+ */
+use proconio::input;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+
+fn main() {
+    input! {
+        n: usize
+    }
+
+    let mut v = vec![vec![]; n + 1];
+    let mut t = vec![0; n + 1];
+    for i in 1..=n {
+        proconio::input! {
+            time: usize,
+            num: usize,
+            a: [usize; num]
+        }
+        t[i] = time;
+        v[i].extend(a);
+    }
+    let used = RefCell::new(vec![false; n + 1]);
+    let mut q: VecDeque<usize> = VecDeque::new();
+    q.push_back(n);
+    used.borrow_mut()[n] = true;
+
+    let mut sum = 0;
+    loop {
+        match q.pop_back() {
+            Some(i) => {
+                sum += t[i];
+                v[i].iter().filter(|&&x| !used.borrow()[x]).for_each(|&x| {
+                    q.push_back(x);
+                    used.borrow_mut()[x] = true; // 一度取得した技は修練しない
+                });
+            }
+            None => break,
+        }
+    }
+
+    println!("{}", sum);
+}
