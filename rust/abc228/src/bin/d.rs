0f278baf3d7a3ee9af304929e2a0582d7e73c7d8
RyotaBannai <ryotala0528@gmail.com>
2022-04-20 19:47:05 +0900

    add: abc228 D ac with BTreeSet

--- rust/abc228/src/bin/d.rs
+++ rust/abc228/src/bin/d.rs
@@ -1,3 +1,49 @@
+use std::collections::BTreeSet;
+
+/**
+ * Liner Probing
+ *
+ * https://atcoder.jp/contests/abc228/tasks/abc228_d
+ *
+ * AC
+ *
+ * SegTree/UnionFind
+ * ・https://atcoder.jp/contests/abc228/submissions/27367620
+ * ・https://atcoder.jp/contests/abc228/submissions/27368971
+ * ・https://atcoder.jp/contests/abc228/submissions/31085677
+ * ・https://atcoder.jp/contests/abc228/submissions/31069281
+ *
+ * Set
+ * ・https://atcoder.jp/contests/abc228/submissions/30610991
+ */
+use proconio::input;
+
 fn main() {
-    todo!();
+    let mo = 1 << 20;
+    input! {
+        n: usize,
+    };
+    let mut v: Vec<isize> = vec![-1; mo];
+    let mut used = (0..mo).collect::<BTreeSet<_>>();
+    for _ in 0..n {
+        input! {
+            q: usize,
+            x: usize
+        }
+        let h = x % mo;
+        if q == 1 {
+            let pos = if let Some(&k) = used.range(h..).next() {
+                k
+            } else {
+                *used.range(..).next().unwrap()
+            };
+
+            used.remove(&pos);
+            v[pos] = x as isize;
+        } else if q == 2 {
+            println!("{}", v[h]);
+        } else {
+            unimplemented!();
+        }
+    }
 }
