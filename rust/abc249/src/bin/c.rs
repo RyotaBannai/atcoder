b2cf3efca31bfcc1b0c42ab62263b5abded015f0
RyotaBannai <ryotala0528@gmail.com>
2022-04-27 22:46:52 +0900

    add: changes for cpg rust support and cargo-compete bind feat

--- rust/abc249/src/bin/c.rs
+++ rust/abc249/src/bin/c.rs
@@ -1,53 +1,4 @@
-/**
- * Just K
- * https://atcoder.jp/contests/abc249/tasks/abc249_c
- *
- * AC
- */
 use std::collections::BTreeMap as Map;
-
-type M = Map<char, usize>;
-
-struct Rec {
-    ss: Vec<String>,
-    n: usize,
-    k: usize,
-}
-impl Rec {
-    fn new(ss: &Vec<String>, k: usize) -> Self {
-        Self {
-            ss: ss.to_owned(),
-            n: ss.len(),
-            k,
-        }
-    }
-    fn dfs(&self, m: &mut M, i: usize) -> usize {
-        if i == self.n {
-            let mut ret = 0;
-            for &v in m.values() {
-                if v == self.k {
-                    ret += 1;
-                }
-            }
-            // dbg!(ret);
-            // dbg!(m);
-            return ret;
-        }
-
-        let m_owned = &mut m.to_owned();
-        // dbg!(&self.ss[i]);
-        self.ss[i].chars().for_each(|x| match m_owned.get_mut(&x) {
-            Some(v) => *v += 1,
-            None => {
-                m_owned.insert(x, 1);
-            }
-        });
-        let a = self.dfs(m_owned, i + 1);
-        let b = self.dfs(m, i + 1);
-
-        std::cmp::max(a, b)
-    }
-}
 
 fn main() {
     proconio::input! {
@@ -55,9 +6,23 @@ fn main() {
         k: usize,
         ss: [String; n],
     };
-    let m = &mut Map::new();
-    let rec = Rec::new(&ss, k);
-    let ans = rec.dfs(m, 0);
+
+    let mut m: Map<char, usize> = Map::new();
+    for s in ss {
+        s.chars().for_each(|x| match m.get_mut(&x) {
+            Some(v) => *v += 1,
+            None => {
+                m.insert(x, 1);
+            }
+        });
+    }
+
+    let mut ans = 0;
+    for &v in m.values() {
+        if v == k {
+            ans += 1;
+        }
+    }
 
     println!("{}", ans);
 }
