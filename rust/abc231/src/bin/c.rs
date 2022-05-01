af11d8495cee4be6ba1fb4d151c9cbf8d4672606
RyotaBannai <ryotala0528@gmail.com>
2022-05-02 00:22:02 +0900

    add: abc231 c counting 2

--- rust/abc231/src/bin/c.rs
+++ rust/abc231/src/bin/c.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+use superslice::*;
+
+/**
+ * Couting 2
+ *
+ * https://atcoder.jp/contests/abc231/tasks/abc231_d
+ */
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        q: usize,
+        mut a: [usize; n],
+        qs: [usize; q],
+    };
+
+    a.sort();
+    let l = a.len();
+    for x in qs {
+        println!("{}", l - a.lower_bound(&x));
+    }
 }
