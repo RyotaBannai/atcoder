74e9cdd67e73052d086527b0d731a4498bd86a0f
RyotaBannai <ryotala0528@gmail.com>
2022-05-03 20:06:02 +0900

    add: abc232 weak takahashi

--- rust/abc232/src/bin/d.rs
+++ rust/abc232/src/bin/d.rs
@@ -1,3 +1,41 @@
+// use itertools::Itertools;
+use proconio::input;
+use proconio::marker::Chars;
+use std::cmp::max;
+use std::collections::VecDeque;
+
+/**
+* Weak Takahashi
+*
+* https://atcoder.jp/contests/abc232/tasks/abc232_d
+
+*/
+
 fn main() {
-    todo!();
+    input! {
+        h:usize,
+        w:usize,
+        grid: [Chars; h],
+    }
+
+    let mut d = vec![vec![0; w]; h];
+    let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+    d[0][0] = 1;
+    q.push_back((0, 0));
+
+    let mut m = 1;
+    while let Some((i, j)) = q.pop_front() {
+        let nd = d[i][j] + 1;
+        // rust 1.42.0 は　iter() をつける.. 1.60 は ok
+        for &(nh, nw) in [(i + 1, j), (i, j + 1)].iter() {
+            // d[nh][nw] != 0 なら計算済
+            if nh < h && nw < w && grid[nh][nw] != '#' && d[nh][nw] == 0 {
+                q.push_back((nh, nw));
+                d[nh][nw] = nd;
+                m = max(m, nd);
+            }
+        }
+    }
+
+    println!("{}", m);
 }
