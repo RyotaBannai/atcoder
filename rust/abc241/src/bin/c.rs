f2489d0d08cfe8e58949ac28fd1c032ebc5f7657
RyotaBannai <ryotala0528@gmail.com>
2022-05-15 23:44:16 +0900

    add: init abc241

---
+++ rust/abc241/src/bin/c.rs
@@ -0,0 +1,11 @@
+// use proconio::{fastout, input};
+// use ac_library_rs::modint::ModInt998244353 as Mint;
+// use derive_new::new;
+// #[derive(new)]
+// use indexmap::indexmap;
+// use std::collections::{BTreeMap, BTreeSet};
+// type Map = BTreeMap<String, usize>;
+
+fn main() {
+    todo!();
+}
