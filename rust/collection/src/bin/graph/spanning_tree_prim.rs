6bbc6f6bb0308bf11b0c5bce42caea13140940a1
RyotaBannai <ryotala0528@gmail.com>
2022-07-13 20:25:45 +0900

    add: prim Memory Limit Exceeded

--- rust/collection/src/bin/graph/spanning_tree_prim.rs
+++ rust/collection/src/bin/graph/spanning_tree_prim.rs
@@ -1,7 +1,7 @@
 /**
  * @cpg_dirspec spanning_tree
  *
- * cpg run -p src/bin/graph/spanning_tree.rs
+ * cpg run -p src/bin/graph/spanning_tree_prim.rs
  */
 // use proconio::{fastout, input, marker::Chars};
 use std::cmp::{max, min};
@@ -10,16 +10,16 @@ use std::cmp::{max, min};
 // use superslice::{self, Ext};
 // use derive_new::new;
 // use indexmap::indexmap;
-// use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{BTreeMap, BTreeSet};
 // type Map = BTreeMap<String, usize>;
-// type Set = BTreeSet<(usize, char)>;
+type Set = BTreeSet<usize>;
 // use easy_ext::ext;
 // use std::collections::{BinaryHeap, VecDeque};
 
 /**
- * 最大流
+ * 最小全域木
  *
- * https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A
+ * https://onlinejudge.u-aizu.ac.jp/problems/GRL_2_A
  *
  * tags: #spanning_tree #全域木 #最小全域木 #prim
  *
@@ -33,6 +33,12 @@ use std::cmp::{max, min};
  *  -> 「特定の頂点を根として捉えて、そこから最小を求めるもの」
  *  単一始点最短経路（single source shortest path: SSSP）: 根からグラフ G の全ての頂点 d への最短経路
  *　全点対間最短経路（all pairs shortest path: APSP）: グラフ G の全ての頂点のペア間の最短経路
+ *
+ *　プリム法
+ * ・根から始めて、根から移動できる頂点への重み（距離）を d へ格納する.
+ * ・処理した箇所は、通過済みとして印をして、二度処理しないようにする.
+ * ・処理する順は、d[i] が小さい順！根から始めた時の次の探索対象の頂点は、根から一番近い頂点となる
+ * ・各の頂点からの探索の際に、それまでに通過したときの d[i] より、短い距離で到達することができるたびに更新していく（探索済みだが、別のルートを通った場合の方が早い場合）
  */
 use std::io;
 
@@ -73,12 +79,16 @@ fn main() {
     let mut u: isize;
     let mut mi;
 
+    let mut s = Set::new();
+    for i in 0..v {
+        s.insert(i);
+    }
     loop {
         mi = inf;
         u = -1;
 
         // d に対して最小を見つける
-        for i in 0..v {
+        for &i in &s {
             if c[i] != Color::Black && mi > d[i] {
                 u = i as isize;
                 mi = d[i];
@@ -90,6 +100,7 @@ fn main() {
         }
 
         c[u as usize] = Color::Black;
+        s.remove(&(u as usize));
 
         for j in 0..v {
             let nd = t[u as usize][j];
