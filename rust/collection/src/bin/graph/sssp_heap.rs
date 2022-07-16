dbb789d6f798e4d2d5e59482ee2f5b8d80db0f92
RyotaBannai <ryotala0528@gmail.com>
2022-07-16 22:50:46 +0900

    add: dijkstra with priority queue wip

--- rust/collection/src/bin/graph/sssp_heap.rs
+++ rust/collection/src/bin/graph/sssp_heap.rs
@@ -1,7 +1,7 @@
 /**
  * @cpg_dirspec sssp
  *
- * cpg run -p src/bin/graph/sssp.rs
+ * cpg run -p src/bin/graph/sssp_heap.rs
  */
 // use proconio::{fastout, input, marker::Chars};
 // use std::cmp::{max, min};
