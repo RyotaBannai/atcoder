4a53494eb1fe028e1ec7a8640d618eea284d7006
RyotaBannai <ryotala0528@gmail.com>
2022-08-08 03:12:51 +0900

    add: abc263 b

--- rust/abc263/src/bin/b.rs
+++ rust/abc263/src/bin/b.rs
@@ -1,4 +1,4 @@
-// use proconio::{fastout, input, marker::Chars};
+use proconio::{fastout, input, marker::Chars};
 // use std::cmp::{
 //     max, min,
 //     Ordering::{Equal, Greater, Less},
@@ -13,8 +13,28 @@
 // type Set = BTreeSet<(usize, char)>;
 // use easy_ext::ext;
 // use std::collections::{BinaryHeap, VecDeque};
+/**
+ * B - Ancestor
+ *
+ * https://atcoder.jp/contests/abc263/tasks/abc263_b
+ *
+ * P 番目の人から、順に先祖を辿る
+ *
+ */
 
-// #[fastout]
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize, // 2<=n
+        p: [usize; n-1]
+    };
+
+    let mut ch = p[n - 2];
+    let mut count = 1;
+    while ch != 1 {
+        ch = p[ch - 2];
+        count += 1;
+    }
+
+    println!("{}", count);
 }
