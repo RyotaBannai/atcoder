8010e523c3aade74a8d324d5b0752059d2cd9b86
RyotaBannai <ryotala0528@gmail.com>
2022-09-06 02:37:27 +0900

    add: polygon-point containment

--- rust/collection/src/bin/geometry/contain_point.rs
+++ rust/collection/src/bin/geometry/contain_point.rs
@@ -1,4 +1,21 @@
 /**
+ * @cpg_dirspec contain_point
+ *
+ * cpg run -p src/bin/geometry/contain_point.rs
+ */
+use std::io;
+
+/**
+ * 多角形-点の包含
+ *
+ * tags: #containments #包含
+ *
+ * 水平線 R の左側にある頂点に対しては、頂点の y でスワップした結果、placement 判定が時計回りになるため交差判定をパスできる
+ * またスワップすることで、非凸（concave, 凹）な多角形でも正しく判定できる
+ *
+ */
+
+/**
  * 計算幾何学パーツ
  */
 use std::cmp::{
@@ -442,6 +459,33 @@ impl PolygonFns {
     }
 }
 
+fn read<T: std::str::FromStr>() -> Vec<T> {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().split(' ').flat_map(str::parse).collect()
+}
+
 fn main() {
-    todo!();
+    let n = read::<usize>()[0];
+    let mut p = vec![];
+    for _ in 0..n {
+        let a = read::<f64>();
+        p.push(Vector::new(a[0], a[1]));
+    }
+
+    let q = read::<usize>()[0];
+
+    for _ in 0..q {
+        let b = read::<f64>();
+        let ans = PolygonFns::contain_point(p.clone(), Vector::new(b[0], b[1]));
+        println!(
+            "{}",
+            match ans {
+                1 => 2,
+                3 => 0,
+                5 => 1,
+                _ => unreachable!(),
+            }
+        );
+    }
 }
