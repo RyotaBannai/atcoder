ad14a79696145158a3943ad864f9d2e8c85325bf
RyotaBannai <ryotala0528@gmail.com>
2022-10-19 21:46:31 +0900

    add: typical90 028

--- rust/typical90/src/bin/028.rs
+++ rust/typical90/src/bin/028.rs
@@ -5,20 +5,21 @@ use proconio::{fastout, input, marker::Chars};
 // use derive_new::new;
 // #[derive(new)]
 // use indexmap::indexmap;
-use std::collections::{BTreeMap, BTreeSet};
+// use std::collections::{BTreeMap, BTreeSet};
 // type Map = BTreeMap<String, usize>;
 // type Set = BTreeSet<(usize, char)>;
 // use easy_ext::ext;
 // use std::collections::{BinaryHeap, VecDeque};
-
-use typical90::query_lib::*;
 
 /**
  *  028 - Cluttered Paper（★4）
  *
  * https://atcoder.jp/contests/typical90/tasks/typical90_ab
  *
- * セグ木を使って更新処理コストを減らす？
+ * tags: #いもす法 #imos
+ *
+ * 参考
+ * https://imoz.jp/algorithms/imos_method.html
  *
 */
 
@@ -30,60 +31,43 @@ fn main() {
     }
 
     let n = 1005;
-
-    let f = |a: isize, b: isize| a + b;
-    let mut vs = vec![];
-    for i in 0..n {
-        vs.push(LazySegTree::new(
-            n,
-            0,
-            0,
-            0,
-            f,
-            f,
-            f,
-            |a: isize, n: usize| a * n as isize,
-            |a: isize, x: isize| a > x,
-        ));
-    }
-
+    let mut v = vec![vec![0isize; n]; n];
+    // 簡単のためinput そのまま使う. 1-index
+    // 入力が左下、右上なので、普通の平面座標とy軸の向きを反対方向にして考える
     for (x1, y1, x2, y2) in s {
-        for i in y1 - 1..y2 - 1 {
-            vs[i].update(x1 - 1, x2 - 1, 1);
-        }
+        v[y1][x1] += 1; // 内側
+        v[y2][x1] -= 1; // 外側 右上
+        v[y2][x2] += 1; // 外側 右下
+        v[y1][x2] -= 1; // 外側 左下
     }
+    // for i in 0..v.len() {
+    //     println!("{:?}", &v[i]);
+    // }
 
-    let mut sum_x = vec![vec![0; n]; n];
-    for i in 0..n {
+    // 行方向に累積和
+    for i in 0..n - 1 {
         for j in 0..n - 1 {
-            sum_x[i][j + 1] = vs[i].query(0, j);
+            v[i][j + 1] += v[i][j];
+        }
+    }
+    // 列方向に累積和
+    for i in 0..n - 1 {
+        for j in 0..n - 1 {
+            v[j + 1][i] += v[j][i];
         }
     }
 
-    // println!("{:?}", &sum_x);
-    // println!("{:?}", &x.dat);
-    // println!("{:?}", &y.dat);
     // 各 k 回重なる部分の総和を求める
     // k: k 回重なる, v: 総和
-    let mut sum: BTreeMap<usize, usize> = BTreeMap::new();
-    for xs in sum_x {
-        for i in 1..n {
-            let v = (xs[i] - xs[i - 1]) as usize;
-            if let Some(x) = sum.get_mut(&v) {
-                *x += 1;
-            } else {
-                sum.insert(v, 1);
-            }
+    let mut sum = vec![0; n];
+    for i in 0..n {
+        for j in 0..n {
+            let k = v[i][j] as usize;
+            sum[k] += 1;
         }
     }
-    // println!("{:?}", &sum);
-
     // i=1 回重なる総和から順位出力
     for i in 1..=k {
-        if let Some(x) = sum.get(&i) {
-            println!("{}", x);
-        } else {
-            println!("0");
-        }
+        println!("{}", sum[i]);
     }
 }
