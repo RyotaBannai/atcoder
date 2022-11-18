36edfd2926c805745303576e61e35df75f256da5
RyotaBannai <ryotala0528@gmail.com>
2022-11-18 22:54:04 +0900

    add: init abc262 B

--- rust/abc262/src/bin/b.rs
+++ rust/abc262/src/bin/b.rs
@@ -1,4 +1,4 @@
-// use proconio::{fastout, input, marker::Chars};
+use proconio::{fastout, input, marker::Chars};
 // use std::cmp::{
 //     max, min,
 //     Ordering::{Equal, Greater, Less},
@@ -8,13 +8,49 @@
 // use derive_new::new;
 // #[derive(new)]
 // use indexmap::indexmap;
-// use std::collections::{BTreeMap, BTreeSet};
-// type Map = BTreeMap<String, usize>;
-// type Set = BTreeSet<(usize, char)>;
+use std::collections::{BTreeMap, BTreeSet, HashSet};
+type Map = BTreeMap<(usize, usize), bool>;
+type Set = HashSet<Vec<usize>>;
 // use easy_ext::ext;
 // use std::collections::{BinaryHeap, VecDeque};
 
-// #[fastout]
+/**
+ * B - Triangle (Easier)
+ *
+ * https://atcoder.jp/contests/abc262/tasks/abc262_b
+ *
+ */
+
+#[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        es: [(usize, usize); m]
+    }
+
+    let mut map = Map::new();
+    for (a, b) in es {
+        map.insert((a, b), true);
+        map.insert((b, a), true);
+    }
+
+    // よくある dfs/bfs で隣接行列で回すやり方だと m=n(n-1)/2 だから 3 つの組み合わせを選ぶことを考えると n=10^2 だから m=10^12 になって TLE
+    // 頂点N は小さいから、N の組み合わせ数を考えると 10^6 でAC できる
+    let mut ans = Set::new();
+    for a in 1..=100 {
+        for b in a + 1..=100 {
+            if map.get(&(a, b)).is_some() {
+                for c in b + 1..=100 {
+                    if map.get(&(b, c)).is_some() && map.get(&(c, a)).is_some() {
+                        let mut v = vec![a, b, c];
+                        v.sort_unstable();
+                        ans.insert(v);
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", ans.len())
 }
