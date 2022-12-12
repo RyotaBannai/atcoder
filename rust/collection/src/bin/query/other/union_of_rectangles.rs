de3e77f78c418bf2a8c72755aafd9d665ac0e222
RyotaBannai <ryotala0528@gmail.com>
2022-12-13 01:15:43 +0900

    add: init abc036

---
+++ rust/collection/src/bin/query/other/union_of_rectangles.rs
@@ -0,0 +1,29 @@
+/**
+ * @cpg_dirspec union_of_rectangles
+ *
+ * cpg run -p src/bin/query/other/union_of_rectangles.rs
+ */
+// use proconio::{fastout, input, marker::Chars};
+// use std::cmp::{min, max};
+// use ac_library_rs::modint::ModInt998244353 as Mint;
+// use superslice::{self, Ext};
+// use derive_new::new;
+// #[derive(new)]
+// use indexmap::indexmap;
+// use std::collections::{BTreeMap, BTreeSet};
+// type Map = BTreeMap<String, usize>;
+// type Set = BTreeSet<(usize, char)>;
+// use easy_ext::ext;
+// use std::collections::{BinaryHeap, VecDeque};
+use library::utils::read::*;
+
+/**
+ * Union of Rectangles
+ *
+ * https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/4/DSL_4_A
+ *
+ */
+
+fn main() {
+    let n = read::<usize>()[0];
+}
