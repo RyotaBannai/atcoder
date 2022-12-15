e47a8bed78022f24f4927ba0713e8559fd1ca43a
RyotaBannai <ryotala0528@gmail.com>
2022-12-15 20:30:58 +0900

    add: Compress Union of Rectangles

--- rust/collection/src/bin/query/other/union_of_rectangles.rs
+++ rust/collection/src/bin/query/other/union_of_rectangles.rs
@@ -3,7 +3,7 @@
  *
  * cpg run -p src/bin/query/other/union_of_rectangles.rs
  */
-// use proconio::{fastout, input, marker::Chars};
+use proconio::{fastout, input, marker::Chars};
 // use std::cmp::{min, max};
 // use ac_library_rs::modint::ModInt998244353 as Mint;
 // use superslice::{self, Ext};
@@ -15,15 +15,81 @@
 // type Set = BTreeSet<(usize, char)>;
 // use easy_ext::ext;
 // use std::collections::{BinaryHeap, VecDeque};
-use library::utils::read::*;
+use library::{structure::compress::*, utils::read::*};
 
 /**
  * Union of Rectangles
  *
  * https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/4/DSL_4_A
  *
+ * tags: #座標圧縮 #compress #累積和 #imos
+ *
+ * 参考
+ * https://algo-logic.info/coordinate-compress/
+ *
  */
 
 fn main() {
-    let n = read::<usize>()[0];
+    input! {
+        n: usize,
+        v: [(isize, isize, isize, isize); n]
+    }
+    // let mut v = vec![];
+    // for _ in 0..n {
+    //     let a = read::<isize>();
+    //     v.push((a[0], a[1], a[2], a[3]));
+    // }
+    let cp2 = compress2(v, false, false);
+    let (compressed, xs, ys) = (cp2.compressed, cp2.xs, cp2.ys);
+
+    let x_len = xs.len();
+    let y_len = ys.len();
+    let mut g = vec![vec![0; x_len + 1]; y_len + 1];
+
+    // println!("{:?}", &compressed);
+
+    // 左上右下
+    // 右下はちょうど外側の角を表すから、内側を求めるときに imos 法の負値としてはそのまま使って良い.
+    // e.g (x,y)=0,0 1,1 の範囲であれば、0,0 のみが面積で、1,1 に負値を設定すれば累積和を取った時にうまく加算できる.
+    for &(x1, y1, x2, y2) in &compressed {
+        g[y1 as usize][x1 as usize] += 1; // 上左
+        g[y1 as usize][x2 as usize] -= 1; // 上右
+        g[y2 as usize][x2 as usize] += 1; // 下右
+        g[y2 as usize][x1 as usize] -= 1; // 下)左
+    }
+
+    // for xs in &g {
+    //     println!("{:?}", xs);
+    // }
+
+    // x軸方向に累積和 (y 軸方向にひとつ多めに流す.)
+    for i in 0..=y_len {
+        for j in 0..x_len {
+            g[i][j + 1] += g[i][j];
+        }
+    }
+    // y軸方向に累積和 (x 軸方向にひとつ多めに流す.)
+    for j in 0..=x_len {
+        for i in 0..y_len {
+            g[i + 1][j] += g[i][j];
+        }
+    }
+
+    // println!();
+    // for xs in &g {
+    //     println!("{:?}", xs);
+    // }
+
+    let mut ans = 0;
+    for i in 0..y_len {
+        for j in 0..x_len {
+            if g[i][j] > 0 {
+                // 圧縮前の隣合う距離の差をx座標とy座標でかけたものを使えば良い
+                // 1~5 ならその間にできるマスは４つ.
+                ans += (xs[j + 1] - xs[j]) * (ys[i + 1] - ys[i]);
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
--- rust/library/src/structure/compress.rs
+++ rust/library/src/structure/compress.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use superslice::{self, Ext};
 
 // １元座標圧縮
-pub fn compress1(xs: Vec<usize>) -> Vec<usize> {
+pub fn compress1(xs: Vec<isize>) -> Vec<isize> {
     // 1. 元の座標上の値をソートしてユニークな値だけ取り出す
     // 2. それぞれの値を二分探索して、新しい位置(index)を探す
     // こうすることで、異なる値が同じ値（index）を取ることなく、値が乗らない座標は削除できる（圧縮できる）
@@ -11,13 +11,62 @@ pub fn compress1(xs: Vec<usize>) -> Vec<usize> {
     let mut nxs = xs;
     for x in &mut nxs {
         let orig_val = *x;
-        *x = vals.lower_bound(&orig_val);
+        *x = vals.lower_bound(&orig_val) as isize;
     }
 
     nxs
 }
 
+#[derive(Clone, Debug)]
+pub struct Compress2 {
+    pub compressed: Vec<(isize, isize, isize, isize)>, // 座標圧縮後の座標
+    pub xs: Vec<isize>,                                // 全x 座標を集めてソートしたvec
+    pub ys: Vec<isize>,                                // 全y 座標を集めてソートしたvec
+}
+
 // 2元座標圧縮
-pub fn compress2(xs: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
-    unimplemented!();
+// 座標の右と下に余白を入れる.
+// input: x1,y1,x2,y2 左上と右下の頂点の座標
+//
+// 入力には以下の３つが想定される(実装は１つ目の入力のみ想定)
+// - 長方形座標. (左上と右下の頂点の座標)
+// - 縦線と横線が与えられる（端点の座標が渡される）
+// - 点(1マス)が与えられる
+pub fn compress2(
+    mut v: Vec<(isize, isize, isize, isize)>,
+    x_buff: bool,
+    y_buff: bool,
+) -> Compress2 {
+    let mut xs = vec![];
+    let mut ys = vec![];
+    for &(x1, y1, x2, y2) in &v {
+        xs.append(&mut vec![x1, x2]);
+        if x_buff {
+            xs.push(x2 + 1);
+        }
+        ys.append(&mut vec![y1, y2]);
+        if y_buff {
+            ys.push(y2 + 1);
+        }
+    }
+
+    xs = xs.into_iter().unique().sorted().collect_vec();
+    ys = ys.into_iter().unique().sorted().collect_vec();
+
+    let n = v.len();
+    for i in 0..n {
+        let (x1, y1, x2, y2) = v[i];
+        let px1 = xs.lower_bound(&x1) as isize;
+        let py1 = ys.lower_bound(&y1) as isize;
+        let px2 = xs.lower_bound(&x2) as isize;
+        let py2 = ys.lower_bound(&y2) as isize;
+
+        v[i] = (px1, py1, px2, py2);
+    }
+
+    Compress2 {
+        compressed: v,
+        xs,
+        ys,
+    }
 }
