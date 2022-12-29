ae77f2e8ed211f0c74cf7970cf6825f3f4f2c238
RyotaBannai <ryotala0528@gmail.com>
2022-12-30 03:37:06 +0900

    add: New Year Tree AC

--- rust/collection/src/bin/other/cf/new_year_tree.rs
+++ rust/collection/src/bin/other/cf/new_year_tree.rs
@@ -1,4 +1,3 @@
-// use itertools::Itertools;
 /**
  * @cpg_dirspec new_year_tree
  *
@@ -10,15 +9,15 @@
 // use superslice::{self, Ext};
 // use derive_new::new;
 // use indexmap::indexmap;
-use std::collections::{BTreeMap, HashSet};
+// use std::collections::{BTreeMap, HashSet};
 // type Map = BTreeMap<String, usize>;
-type Set = HashSet<isize>;
+// type Set = HashSet<isize>;
 // use std::collections::{BinaryHeap, VecDeque};
 use library::{
     graph::{euler_tour::*, vertex::*},
     query::seg_tree::*,
-    utils::read::*,
-    *,
+    scan,
+    utils::{scanner::Scanner, writer::Writer},
 };
 
 /**
@@ -28,121 +27,63 @@ use library::{
  *
  * tags: #EulerTour #オイラーツアー #セグ木 #segment_tree
  *
- * MLE テストケース #50 まではpass
- *
  */
 
 // #[fastout]
 fn main() {
-    let a = read::<usize>();
-    let (n, q) = (a[0], a[1]);
-    let c = read::<usize>(); // color
+    let mut sc = Scanner::stdin();
+    let mut wr = Writer::stdout();
+    let (n, q) = scan!(sc, usize, usize);
+    let c = scan!(sc, i8; n); // color
     let mut list = vec![vec![]; n + 1];
-    for i in 1..n {
+    for _ in 1..n {
         // i-index で管理
-        let b = read::<usize>();
-        let (s, t) = (b[0], b[1]);
+        let (s, t) = scan!(sc, usize, usize);
         list[s].push(Vertex::new(s, t, 0)); // 初め重みは全て0
         list[t].push(Vertex::new(t, s, 0));
     }
 
-    // for xs in list.iter() {
-    //     println!("{:?}", &xs);
-    // }
-
-    let et = euler_tour(Vertex::new(0, 1, 0), list.clone());
-
-    // println!("depth {:?}", &et.depth);
-    // println!("visit {:?}", &et.visit);
-    // println!("vcost1 {:?}", &et.vcost1);
-    // println!("vcost2 {:?}", &et.vcost2);
-    // println!("i {:?}", &et.i);
-    // println!("o {:?}", &et.o);
-    // println!("o2 {:?}", &et.o2);
-    // println!();
+    let et = euler_tour(Vertex::new(0, 1, 0), list);
 
     // RSQ_RAQ
     let mut seg = LazySegTree::new(
         et.vcost2.len(),
-        Set::new(),
-        Set::new(),
+        0u64,
+        0u64,
         0,
         0,
-        |mut a: Set, mut b: Set| a.union(&mut b).cloned().collect::<Set>(),
-        // この区間は全部x
-        |s: Set, x: isize| {
-            let mut ret = Set::new();
-            ret.insert(x);
-            ret
-        },
-        |_: isize, b: isize| b, // update(replace)
+        |a: u64, b: u64| a | b,          // or
+        |_: u64, x: isize| 1 << (x - 1), // この区間は全部x
+        |_: isize, b: isize| b,          // replace
         |a: isize, _: usize| a,
-        |a: Set, x: Set| unimplemented!(),
+        |_: u64, _: u64| unimplemented!(),
     );
 
     for (i, u) in et.visit.iter().enumerate() {
-        let mut s = Set::new();
-        s.insert(c[u - 1] as isize); // 色を初期値にする
-        seg.set(i, s);
+        seg.set(i, 1 << (c[u - 1] - 1)); // 色を初期値にする
     }
     seg.build();
 
-    // println!("{:?}", &seg.dat);
-    // println!();
-
     // read query
     for _ in 0..q {
-        let b = read::<usize>();
-        if b[0] == 2 {
-            let v = b[1] - 1;
+        let m = scan!(sc, usize);
+        if m == 2 {
+            let v = scan!(sc, usize) - 1;
             let i = et.i[v];
             let o2 = et.o2[v];
-            let or = seg.query(i, o2);
-            println!("{}", or.len());
+            let mut num = seg.query(i, o2);
+            wr.write(num.count_ones()).unwrap();
         } else {
-            let (v, x) = (b[1] - 1, b[2] as isize);
-
-            // 頂点ごと部分木に更新をかける.
+            let (v, x) = (scan!(sc, usize) - 1, scan!(sc, isize));
             let i = et.i[v];
             let o2 = et.o2[v];
             seg.update(i, o2 + 1, x);
 
             // 意図的にセグ木のlazy を更新.
-            // println!("{:?}", v + 1);
-            // println!("{:?}", &list[v + 1]);
             // seg.force_update();
-
             // 葉の状態を表示.
-            // println!("{:?}", &seg.dat);
-            // let nn = seg.n;
-            // println!("visit {:?}", &et.visit);
             // seg.show_leafs();
             // println!();
         }
     }
 }
-
-/*
-10 10
-23 25 23 42 23 53 49 40 28 44
-1 7
-1 2
-2 4
-4 10
-8 10
-6 8
-3 8
-5 3
-9 5
-2 10
-1 6 52
-1 8 43
-2 3
-1 4 39
-1 8 44
-1 9 39
-2 1
-2 4
-1 6 36
-
-*/
